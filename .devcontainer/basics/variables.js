aec1cf2a7a00dbc04280c2645d9bb053beb72ad3
swapnil884 <88192516+swapnil884@users.noreply.github.com>
2024-02-02 17:28:39 +0000

    commit

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 161220000
+let accountEmail = "swapnil@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 (Not allowed)
+
+accountEmail = "swapnil16@gmail.com"
+accountPassword = "01234"
+accountCity = "Bangalore"
+
+/* var is not preferrably used because of functional scope and block scope issues */
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
