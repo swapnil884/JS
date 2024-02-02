e7fdd9250f11ea1116a541f930bddb3f79c093fd
swapnil884 <88192516+swapnil884@users.noreply.github.com>
2024-02-02 17:58:20 +0000

    dataTypes

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treats all js code as newer version
+
+// alert (3+3) => we are using node js, not browser
+
+console.log(3 
+    + 3); // code readability
+
+let name = "Swapnil"
+let age = 23
+let isLoggedIn = false
+let state 
+
+// number => 2 raised to the power 55
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique
+// object
+
+console.log(typeof "Swapnil")
+console.log(typeof age)
+
+console.table([name, age, isLoggedIn, state ])
